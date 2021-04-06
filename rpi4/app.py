2131d90de7542dda88359c2eb1c65d8e1680a53d
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-04-06 13:31:48 -0700

    make servo var global

--- rpi4/app.py
+++ rpi4/app.py
@@ -7,14 +7,14 @@ URL = 'http://doranelle.kevinhsu.net:5000/websockets'
 sio = socketio.Client()
 
 isOn = False
+servo1 = null
 
-def setup(): 
-    GPIO.setmode(GPIO.BOARD)
-    # Set pin 11 as an output, and set servo1 as pin 11 as PWM
-    GPIO.setup(11,GPIO.OUT)
-    servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
-    servo1.start(0)
-    print("GPIO setup complete, pulse set to zero")
+GPIO.setmode(GPIO.BOARD)
+# Set pin 11 as an output, and set servo1 as pin 11 as PWM
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+servo1.start(0)
+print("GPIO setup complete, pulse set to zero")
 
 @sio.event
 def connect():
@@ -27,6 +27,7 @@ def disconnect():
 @sio.on('pi_do')
 def pi_do(data):
     global isOn
+    global servo1
     print('I received a message!')
     print("raspberry pi received message, do: " + data["message"])
     if isOn: 
@@ -36,7 +37,7 @@ def pi_do(data):
     isOn = not isOn
 
 
-setup()
+#  setup()
 sio.connect(URL)
 sio.wait()
 
