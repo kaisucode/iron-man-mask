e8ff48c091ba32dd08ba875f578ddf06f8b3e45a
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-04-14 14:31:37 -0700

    Add debug

--- servoTest/app.py
+++ servoTest/app.py
@@ -2,15 +2,6 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11, 50)
-servo1.start(0) # start duty cycle at zero
-
-print("intialized at down position")
-
-
-
 def setAngle(angle):
     global servo1
     duty = angle / 18 + 3
@@ -20,9 +11,20 @@ def setAngle(angle):
     servo1.ChangeDutyCycle(0)
 
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11, 50)
+servo1.start(0) # start duty cycle at zero
+
+print("intialized at down position, starting rotation in 3 seconds")
+
+time.sleep(3)
 print("test move up")
 setAngle(90)
 
+
+print("moving down in 3 seconds")
+time.sleep(3)
 print("test move down")
 setAngle(0)
 
